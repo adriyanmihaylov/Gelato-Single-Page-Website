8d08ab42800a9e3aede70852b80483308b83eb61
adriyanmihaylov@yahoo.com <adriyanmihaylov@yahoo.com>
2018-06-28 10:35:44 +0300

    initial-commit

---
+++ script/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+// $(".navigation").on("click", function() {
+//     $(this).html("Clicked");
+//     $(this).hide(100);
+//     $(this).show(100);
+//     var red = getRandom(255);
+//     var green = getRandom(255);
+//     var blue = getRandom(255);
+
+//     $(this).css("background-color","rgb(" + red + "," + green + "," + blue + ")");
+// })
+
+// $(document).ready(function(){
+//     $(".navigation").mouseover(function(){
+//         $(this).css("opacity", "0.9");
+//     });
+//   });
+
+//   function getRandom(max) {
+//     var number = Math.random() * max;
+//     return number | 0;
+//   }
